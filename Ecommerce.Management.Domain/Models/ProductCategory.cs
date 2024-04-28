df5dcabc720962577e79da516c050cb97511ba31
SridharElumalai <sridharonstar@gmail.com>
2024-04-28 15:58:41 +0530

    Alter the ProductCategory

--- Ecommerce.Management.Domain/Models/ProductCategory.cs
+++ Ecommerce.Management.Domain/Models/ProductCategory.cs
@@ -4,9 +4,7 @@ namespace Ecommerce.Management.Domain.Models
     public partial class ProductCategory
     {
         public Guid Id { get; set; }
-        public Guid? ParentCategoryId { get; set; }
         public string Category_Name { get; set; }
-        public ProductCategory ParentCategory { get; set; }
 
     }
 }
--- Ecommerce.Management.Domain/Response/Queries/Product/GetProductByIdResponseModel.cs
+++ Ecommerce.Management.Domain/Response/Queries/Product/GetProductByIdResponseModel.cs
@@ -10,10 +10,10 @@ namespace Ecommerce.Management.Domain.Response.Queries.Product
     public class GetProductByIDResponseModel
     {
         public Guid Id { get; set; }
-        public Guid Category_Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Product_Image { get; set; }
+        public Guid Category_Id { get; set; }
         public ProductCategory Category { get; set; }
 
     }
